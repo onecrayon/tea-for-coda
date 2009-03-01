876ad9d040f9805e89b9bd40f145ad5dd5597302
Ian Beck <ian@onecrayon.com>
2009-03-01 07:22:17 -0800

    Merged in setup.py improvements from Espresso project

--- setup.py
+++ setup.py
@@ -5,12 +5,37 @@
 
 from distutils.core import setup
 import py2app
+import os
+
+
+def include_files(path):
+    '''
+    Walks a given folder and returns a list of its contents
+    
+    List includes the folder's name (so that we can use it with data_files)
+    '''
+    files = []
+    for root, dirs, filenames in os.walk(path):
+        if filenames:
+            for file in filenames:
+                # Don't include hidden files
+                if (file[0] != '.'):
+                    files.append(os.path.join(root, file))
+    return files
+
+# Configure this by hand for any included directories
+includes = [
+#     ('../../TEA', include_files('./TEA')),
+#     ('./', include_files('./Resources')),
+]
 
 setup(
     name='TEA for Coda',
     plugin = ['TEAforCoda.py'],
+    data_files = includes,
     options=dict(py2app=dict(
         extension='.codaplugin',
         semi_standalone = True,
+        site_packages = True,
     )),
 )
