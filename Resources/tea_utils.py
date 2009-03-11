a1453203d409018cf300995fab0edefba03c25f0
Ian Beck <ian@onecrayon.com>
2009-03-10 18:05:20 -0700

    Added skeleton for loading actions

---
+++ Resources/tea_utils.py
@@ -0,0 +1,11 @@
+'''
+This module includes common utility functions for working with
+TEA actions, such as locating and loading them
+'''
+
+import imp
+import sys
+import os.path
+
+def setup_actions():
+    pass
--- TEAforCoda.py
+++ TEAforCoda.py
@@ -8,22 +8,35 @@ from Textmate into Coda
 from Foundation import *
 import objc
 
+from tea_utils import *
+
 NSObject = objc.lookUpClass('NSObject')
 
 class TEAforCoda(NSObject):
-    '''Docstring here'''
+    '''
+    Initializes the menu items and is responsible for directing
+    actions to the appropriate Python script
+    '''
     
-    def initWithPlugInController_bundle_(self, inController, yourBundle):
+    def initWithPlugInController_bundle_(self, inController, thisBundle):
         '''Required method; run when the plugin is loaded'''
         self = super(TEAforCoda, self).init()
         if self is None: return None
         
         # Set object's internal variables
         self.controller = inController
-        # TODO: Setup the menu items here
+        # Setup the menu items
+        setup_actions()
+        
+        # Add the Resources folder to the path
+        sys.path.append(thisBundle.bundlePath() + '/Contents/Resources/')
         
         return self
     
     def name(self):
         '''Required method; returns the name of the plugin'''
         return 'TEA for Coda'
+    
+    def act(self, caller):
+        '''Runs the selected action'''
+        pass
