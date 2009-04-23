24c7e1ecd4d08064250989595fb5d2aedc0f9f82
Ian Beck <ian@onecrayon.com>
2009-04-23 08:28:31 -0700

    Added basic skeleton for loading a sheet

--- src/Contents/Resources/TEAWrapSelectionInTag.py
+++ src/Contents/Resources/TEAWrapSelectionInTag.py
@@ -1,8 +1,28 @@
 '''Wraps the currently selected text in a tag of the user's choice'''
 
 from Foundation import *
+from AppKit import *
+from PyObjCTools import AppHelper
+import objc
 
-class TEAWrapSelectionInTag(NSObject):
+class TEAWrapSelectionInTag(NSWindowController):
+    prefix = objc.IBOutlet()
+    suffix = objc.IBOutlet()
+    default = objc.IBOutlet()
+    closetag = objc.IBOutlet()
+    
+    @objc.IBAction
+    def doSubmitSheet_(self, sender):
+        NSApp.endSheet_returnCode_(self.customSheet, 1)
+    
+    @objc.IBAction
+    def cancel_(self, sender):
+        NSApp.endSheet_returnCode_(self.customSheet, 0)
+    
+    @AppHelper.endSheetMethod
+    def didEndSheet_returnCode_contextInfo_(self, sheet, code, info):
+        sheet.orderOut_(self)
+    
     def act(self, controller):
         NSLog('Hello world!')
         pass
